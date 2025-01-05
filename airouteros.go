3c9b3fcd59ec8871b715acde0a947e804ca5649a
Kurniaji Gunawan <kurniaji.gunawan@gmail.com>
2025-01-05 14:15:17 +0700

    fix prevent panic on close without connecting

--- airouteros.go
+++ airouteros.go
@@ -2,6 +2,7 @@ package airouteros
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/go-routeros/routeros/v3"
 )
@@ -9,6 +10,8 @@ import (
 type RouterOS struct {
 	client *routeros.Client
 	option *Options
+
+	isConnected bool
 }
 
 func NewRouterOS(opt *Options) *RouterOS {
@@ -17,20 +20,40 @@ func NewRouterOS(opt *Options) *RouterOS {
 	}
 }
 
+// NewAndConnctRouterOS creates a new RouterOS client and connects to the router.
+func NewAndConnctRouterOS(opt *Options) (*RouterOS, error) {
+	router := &RouterOS{
+		option: opt,
+	}
+	return router, router.Connect()
+}
+
 // Connect connects to the router.
 func (r *RouterOS) Connect() error {
 	var err error
 	if r.option.Timeout > 0 {
 		r.client, err = routeros.DialTimeout(r.option.Address, r.option.Username, r.option.Password, r.option.Timeout)
-		return err
+		if err != nil {
+			return err
+		}
+		r.isConnected = true
+		return nil
 	}
 	r.client, err = routeros.Dial(r.option.Address, r.option.Username, r.option.Password)
-	return err
+	if err != nil {
+		return err
+	}
+	r.isConnected = true
+	return nil
 }
 
 // Close the connection to the router.
-func (r *RouterOS) Close() {
+func (r *RouterOS) Close() error {
+	if !r.isConnected {
+		return fmt.Errorf("router is not connected")
+	}
 	r.client.Close()
+	return nil
 }
 
 // Async starts asynchronous mode and returns immediately..
@@ -39,11 +62,14 @@ func (r *RouterOS) Async() {
 }
 
 // Run runs a sentence on the router.
-func (b *RouterOS) CallContext(ctx context.Context, sentence ...string) (*routeros.Reply, error) {
-	return b.client.RunContext(ctx, sentence...)
+func (r *RouterOS) CallContext(ctx context.Context, sentence ...string) (*routeros.Reply, error) {
+	if !r.isConnected {
+		return nil, fmt.Errorf("router is not connected")
+	}
+	return r.client.RunContext(ctx, sentence...)
 }
 
 // GetQuery returns the query of the builder.
-func (b *RouterOS) GetPath() string {
+func (r *RouterOS) GetPath() string {
 	return `/`
 }
